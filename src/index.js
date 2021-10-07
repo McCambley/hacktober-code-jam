affaab1d7b25a43d3590e52d1f3700832b5e0a65
McCambley <jakemccamb16@gmail.com>
2021-10-07 15:48:35 -0400

    Fix: correct routing link

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/hacktober-code-jam">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
