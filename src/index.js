94628af0c3c2cf8d9885b8643a5f9def485aea81
McCambley <jakemccamb16@gmail.com>
2021-10-21 16:10:36 -0400

    Fix: remove basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/hacktober-code-jam">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
