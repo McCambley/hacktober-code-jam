e523860fe85ecbb57d2d8e51d93379880f06d635
McCambley <jakemccamb16@gmail.com>
2022-01-09 20:17:51 -0500

    Ref: move Place styles

--- src/components/place/Place.js
+++ src/components/place/Place.js
@@ -1,81 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-// import { Link } from "react-router-dom";
-
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 255px 1fr;
-  gap: 16px;
-  margin-bottom: 20px;
-
-  &:last-of-type {
-    margin-bottom: 0;
-  }
-
-  @media (max-width: 1440px) {
-    grid-template-columns: 210px 1fr;
-  }
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-const ImageDiv = styled.div`
-  cursor: pointer;
-  transition: opacity 0.15s ease;
-  border-radius: 24px;
-  overflow: hidden;
-  background-image: url(${(props) => props.image});
-  background-position: center;
-  background-size: cover;
-  min-height: 180px;
-  min-width: 100%;
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-// const Image = styled.img`
-//   width: 100%;
-// `;
-const TextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-`;
-const Title = styled.h3`
-  margin: 0;
-  font-family: "Bitter", serif;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 32px;
-  margin-bottom: 10px;
-  text-decoration: none;
-  color: inherit;
-  transition: opacity 0.15s ease;
-  cursor: pointer;
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  @media (max-width: 1440px) {
-    font-size: 18px;
-    line-height: 24px;
-  }
-`;
-const Subtitle = styled.p`
-  margin: 0;
-  font-weight: 400;
-  font-size: 24px;
-  line-height: 24px;
-  @media (max-width: 1440px) {
-    font-size: 18px;
-    line-height: 24px;
-  }
-`;
+import {
+  Container,
+  ImageDiv,
+  TextWrapper,
+  Title,
+  Subtitle,
+} from "./styledPlace";
 
 export default function place({
   place,
